04dd9c38070ee320975fb7d85e988ea2f7bf84db
MontseCamch <montserratcamch@gmail.com>
2024-08-01 13:52:07 -0600

    actualizacion de ejercicios

--- arrays-and-loops.js
+++ arrays-and-loops.js
@@ -13,6 +13,40 @@ const halves = [];
 let num = 10;
 let half = num / 2;
 console.log(halves.push(half, half));
-console.log(halves)
+console.log(halves);
 
 //Secret Society
+const secretName = ['Harry', 'Ron', 'Hermione'];
+let name;
+let secret = [];
+
+for(let i = 0; i <= secretName.length -1; i++) {
+    name = secretName[i];
+    character= JSON.stringify(name);
+    let letter = character.charAt(1);
+    console.log(secret.push(letter));
+    secret.sort();
+
+}
+console.log(secret);
+
+//Online status
+const onlineStatus = ['mockIng99', 'J0eyPunch', 'glassedFer', 'tutti20'];
+
+if(onlineStatus.length > 2 ) {
+    console.log(`${onlineStatus.slice(0, 2)} and ${onlineStatus.length - 2} more online`);
+}
+
+
+//Array of Multiples
+const number = 17;
+const lim = 6;
+let accum = 0;
+const multiples = [];
+for(let i = 1; i <= lim; i++){
+    accum = accum + number;
+    multiples.push(accum);
+}
+console.log(multiples);
+
+//Positive dominance in Array
